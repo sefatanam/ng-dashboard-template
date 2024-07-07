8490f3ac762a58fc0826cf4563fbdec94992e737
sefatanam <sefatanam@gmail.com>
2024-07-07 17:55:21 +0600

    material light and dark theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [
+		RouterOutlet,
+		MatButtonModule,
+		MatIconModule,
+		MatDividerModule,
+		MatExpansionModule,
+		MatFormFieldModule,
+		MatDatepickerModule,
+		MatMenuModule,
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
 export class AppComponent {
 	title = 'dive-into-angular';
+	accordion = viewChild.required(MatAccordion);
 }

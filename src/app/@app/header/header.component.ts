8808c1625b5130762d8a3fcb625b27c0a08fb2b2
sefatanam <sefatanam@gmail.com>
2024-07-09 17:51:31 +0600

    feat: header component design

--- src/app/@app/header/header.component.ts
+++ src/app/@app/header/header.component.ts
@@ -1,9 +1,35 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatAnchor, MatButton, MatIconButton } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatFormField, MatPrefix } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatTooltip } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [],
+	imports: [
+		MatIcon,
+		MatIconButton,
+		AsyncPipe,
+		MatFormField,
+		MatInput,
+		MatPrefix,
+		MatBadge,
+		MatMenu,
+		MatMenuTrigger,
+		MatMenuItem,
+		MatDivider,
+		MatButton,
+		MatTooltip,
+		RouterLink,
+		MatAnchor,
+	],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })

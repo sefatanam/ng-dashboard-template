8eae7f5e9ab531764c3e07b5c498fd29338b88b2
sefatanam <sefatanam@gmail.com>
2024-07-12 00:00:36 +0600

    feat: toggle sidebar

--- src/app/@app/header/header.component.ts
+++ src/app/@app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatAnchor, MatButton, MatIconButton } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
@@ -9,6 +9,8 @@ import { MatInput } from '@angular/material/input';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { MatTooltip } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
+import { LayoutApiService } from '../../../../projects/components/src';
+import { ThemeManagerService } from '../../../../projects/components/src/lib/_services/theme-manager.service';
 
 @Component({
 	selector: 'app-header',
@@ -32,5 +34,25 @@ import { RouterLink } from '@angular/router';
 	],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
+	host: {
+		// eslint-disable-next-line prettier/prettier
+		class: 'block w-full h-full',
+	},
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	protected _themeManager = inject(ThemeManagerService);
+	private _layoutApi = inject(LayoutApiService);
+
+	@Input()
+	sidebarHidden = false;
+
+	toggleSidebar(): void {
+		if (!this.sidebarHidden) {
+			this._layoutApi.hideSidebar('root');
+		} else {
+			this._layoutApi.showSidebar('root');
+		}
+
+		this.sidebarHidden = !this.sidebarHidden;
+	}
+}
